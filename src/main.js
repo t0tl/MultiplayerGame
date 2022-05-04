69cb5b45ae929a83cb284af9025c2c7b14cf4950
t0tl <timothy.lindbloma@gmail.com>
2022-05-04 21:57:57 +0200

    Added wasd keys, snake restarts if wall,
    Added a reset button, implemented so that one can use both arrow keys and WASD. Moreover, if the snake hits the walls, the game ends and you can restart by pressing any keys.

--- src/main.js
+++ src/main.js
@@ -1,78 +1,118 @@
-window.onload=function() {
-    canvas = document.querySelector('canvas'); // Select canvas
-    ctx = canvas.getContext("2d"); // Select the context for the canvas
-    document.addEventListener("keydown", keyPush); // Listen for keyboard presses.
-    setInterval(game,100); // Run function game every 100 ms.
+"use strict";
+var x = document.querySelector("#gameStateID");
+const restartGame = function() {
+    x.style.display = "block";
+    px = 10;
+    py = 10;
+    gs = 20;
+    tc = 20;
+    xv = 0;
+    yv = 0;
+    trail = []; // Array holding objects
 }
-px = py = 10; // Player position
-gs = tc = 20; // gs = graphic scaling, tc = tile count
-xv = yv = 0; // x and y velocity
-trail=[]; // Array holding objects
-//tail=5; // Length of snake
+
+window.onload = function () {
+    canvas;
+    ctx;
+    document.addEventListener("keydown", keyPush); // Listen for keyboard presses.
+    setInterval(game, 100); // Run function game every 100 ms.
+  };
+
+let px = 10;
+let py = 10;
+let gs = 20;
+let tc = 20;
+let xv = 0;
+let yv = 0;
+let trail = []; // Array holding objects
+
+const canvas = document.querySelector("canvas"); // Select canvas
+const ctx = canvas.getContext("2d"); // Select the context for the canvas
+const btn = document.querySelector("#gameStateButton").addEventListener("click", restartGame); // Select restart button
 
 function game() {
-    px+=xv; // Change snake x position by x velocity
-    py+=yv; // Change snake y position by y velocity
-    if (px<0) { 
-        //px = tc-1;
-        return;
-    }
-    if (px>tc-1) {
-        //px = 0;
-        return;
-    }
-    if (py<0) {
-        //py = tc-1;
-        return;
-    }
-    if (py>tc-1) {
-        //py = 0;
-        return;
-    }
-    // Background
-    ctx.fillStyle="black"; 
-    ctx.fillRect(0,0,canvas.width,canvas.height);
-    
-    // Render snake.
-    ctx.fillStyle="lime";
-    for(var i=0; i<trail.length; i++) {
-        // Draw each piece of the snake.
-        ctx.fillRect(trail[i].x*gs,trail[i].y*gs,gs-2,gs-2);
-        if (trail[i].x == px && trail[i].y==py) {
-            //Put different screen here 
-            //and tell to press key to begin
-            //ctx.fillStyle="black";
-            //ctx.fillRect(0,0,canvas.width,canvas.height);
-        }
-    }
-    // Adds a tile object to the trail array.
-    trail.push({x:px,y:py});
+  px += xv; // Change snake x position by x velocity
+  py += yv; // Change snake y position by y velocity
+  if (px < 0) {
+    //px = tc-1;
+    restartGame();
+  }
+  if (px > tc - 1) {
+    //px = 0;
+    restartGame();
+  }
+  if (py < 0) {
+    //py = tc-1;
+    restartGame();
+  }
+  if (py > tc - 1) {
+    //py = 0;
+    restartGame();
+  }
 
-    // Make snake non-continous
-    /*
-    while(trail.length>tail) {
-        trail.shift();
+  // Background
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  // Render snake.
+  ctx.fillStyle = "lime";
+  for (var i = 0; i < trail.length; i++) {
+    // Draw each piece of the snake.
+    ctx.fillRect(trail[i].x * gs, trail[i].y * gs, gs - 2, gs - 2);
+    if (trail[i].x == px && trail[i].y == py) {
+        //Insert flood fill algo.
+      restartGame();
     }
-    */
+  }
+  // Adds a tile object to the trail array.
+  trail.push({ x: px, y: py });
 }
 
 function keyPush(event) {
-    switch(event.keyCode) {
-        // Left arrow
-        case 37:
-            xv=-1;yv=0;
-            break;
-        // Down arrow
-        case 38:
-            xv=0;yv=-1;
-            break;
-        // Right arrow
-        case 39:
-            xv=1;yv=0;
-            break;
-        // Up arrow 
-        case 40:
-            xv=0;yv=1;
-            break;
-    }
+  switch (event.keyCode) {
+    // Left arrow
+    case 37:
+        xv = -1;
+        yv = 0;
+        x.style.display = "none";
+        break;
+    // Down arrow
+    case 38:
+        xv = 0;
+        yv = -1;
+        x.style.display = "none";
+        break;
+    // Right arrow
+    case 39:
+        xv = 1;
+        yv = 0;
+        x.style.display = "none";
+        break;
+    // Up arrow
+    case 40:
+        xv = 0;
+        yv = 1;
+        x.style.display = "none";
+        break;
+    case 65: //a
+        xv = -1;
+        yv = 0;
+        x.style.display = "none";
+        break;
+    case 87: //s
+        xv = 0;
+        yv = -1;
+        x.style.display = "none";
+        break;
+    case 68: //d
+        xv = 1;
+        yv = 0;
+        x.style.display = "none";
+        break;
+    case 83: //w
+        xv = 0;
+        yv = 1;
+        x.style.display = "none";
+        break;
+  }
 }
