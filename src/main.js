d1b9df10095f6d8effada6a0d7ffee5985c3d51a
joel90688 <joel90688@gmail.com>
2022-05-11 15:08:36 +0200

    Add getEnclosedShape

--- src/main.js
+++ src/main.js
@@ -8,7 +8,9 @@ const restartGame = function() {
     yv = 0;
     trail = []; // Array holding limbo-tiles
     filled = []; // Array holding filled tiles
-    filled.push({ x: px + 1, y: py + 1}, { x: px + 1, y: py}, { x: px + 1, y: py - 1}, { x: px, y: py + 1}, { x: px, y: py - 1}, { x: px - 1, y: py + 1}, { x: px - 1, y: py}, { x: px - 1, y: py - 1},{ x: px, y: py});
+    filled.push({ x: px + 1, y: py + 1}, { x: px + 1, y: py}, { x: px + 1, y: py - 1}, { x: px, y: py + 1}, { x: px, y: py - 1}, { x: px - 1, y: py + 1}, { x: px - 1, y: py}, { x: px - 1, y: py - 1});
+    const edges = [...filled];
+    filled.push({ x: px, y: py})
   }
 
 window.onload = function () {
@@ -76,13 +78,65 @@ function game() {
     getInsideTile();
   }else{
     trail.push({ x: px, y: py });
+    edges.push({ x: px, y: py });
   }
 }
 
+<<<<<<< Updated upstream
 function getNeighbors(node){
   let neighbors = [];
   for(let i = 0; i < edges.length; )
   return neighbors;
+=======
+function getEnclosedShape(){
+  let startNode = trail
+
+}
+
+// checks if a given point is on the trail.
+function matchesTrail(pos_x, pos_y) {
+  for (var i = 0; i < trail.length; i++) {
+    if (trail[i].x == pos_x && trail[i].y == pos_y) {
+        return true;
+    }
+  }
+
+  return false;
+}
+
+// checks if a given point is on the filled area.
+function matchesFilled(pos_x, pos_y) {
+  for (var i = 0; i < filled.length; i++) {
+    if (filled[i].x == pos_x && filled[i].y == pos_y) {
+        return true;
+    }
+  }
+
+  return false;
+}
+
+// Fills zone from a given starting point
+function flood_fill(pos_x, pos_y) {
+
+  // if there is no wall or if I haven't been there
+  if(matchesFilled(pos_x, pos_y) || matchesTrail(pos_x, pos_y)) 
+     return;                                              
+  
+  filled.push({x:pos_x, y:pos_y})  
+  
+  flood_fill(pos_x + 1, pos_y);  // then i can either go south
+  flood_fill(pos_x - 1, pos_y);  // or north
+  flood_fill(pos_x, pos_y + 1);  // or east
+  flood_fill(pos_x, pos_y - 1);  // or west
+  
+  return;
+}
+function nodesMatch(node1, node2, xinc, yinc) {
+  if (node1.x == node2.x+xinc && node1.y == node2.y+yinc) {
+    return true;
+  }
+  return false;
+>>>>>>> Stashed changes
 }
 
 function getInsideTile(){
